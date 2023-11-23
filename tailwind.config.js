590c7e0fa8953cc21746ef6696961ac3190c3727
cc-greatman <blessedgreatman96@gmail.com>
2023-11-23 07:51:12 +0100

    First Commit and Opened Repo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'primary': '#001560',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
