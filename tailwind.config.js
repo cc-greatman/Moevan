a85ec1c6ebaffb747b805eec013b78d66a5b7a0c
cc-greatman <blessedgreatman96@gmail.com>
2023-12-07 08:58:14 +0100

    Issue: Screen adaptability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
       'primary': '#001560',
+      'gradient-1': '#000E3E',
+      'gradient-2': '#001D82',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexend: ['"Lexend"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
